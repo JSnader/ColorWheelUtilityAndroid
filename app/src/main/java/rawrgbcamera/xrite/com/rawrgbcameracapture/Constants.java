de466521657de8f71e028a800ffe9fc86a40b038
jsnader <jsnader@xrite.com>
2016-12-08 11:48:13 -0500

    Adding files in project 'RawRgbCameraCapture' to Git...

---
+++ app/src/main/java/rawrgbcamera/xrite/com/rawrgbcameracapture/Constants.java
@@ -0,0 +1,9 @@
+package rawrgbcamera.xrite.com.rawrgbcameracapture;
+
+/**
+ * Created by jsnader on 12/6/16.
+ */
+
+public class Constants {
+    public final static String LOG_TAG = "RgbRawCam";
+}
